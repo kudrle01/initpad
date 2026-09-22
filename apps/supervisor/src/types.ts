2a68a4302bc3d48c87ec06dc5dd0419e33927f0d
Jan Kudrlička <kudrlicka.jan@gmail.com>
2026-09-22 14:15:50 +0200

    chore(release): prepare platform 0.2.7

--- apps/supervisor/src/types.ts
+++ apps/supervisor/src/types.ts
@@ -1,4 +1,4 @@
-export const SUPERVISOR_VERSION = '0.2.6';
+export const SUPERVISOR_VERSION = '0.2.7';
 
 export type UpdateOperationStatus = 'accepted' | 'running' | 'succeeded' | 'failed' | 'rolled-back';
 
